acdfcd56257c2bdc2074282d6ba03e982e7ada9e
Rohit <rohitamar@gmail.com>
2023-05-12 21:34:43 -0400

    dam man

--- test.js
+++ test.js
@@ -1,20 +1,22 @@
 const WebSocket = require('ws');
 
-const socket = new WebSocket('wss://ws.finnhub.io?token=chc7irhr01quf0103vn0chc7irhr01quf0103vng');
+require('dotenv').config();
 
-// Connection opened -> Subscribe
+const API_URL = 'wss://ws.finnhub.io?token=' + process.env.API_KEY;
+
+const socket = new WebSocket(API_URL);
+
 socket.addEventListener('open', function (event) {
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'MSFT'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'META'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'VDC'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'NFLX'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'TGT'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'GOOGL'}))
-    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AMZN'}))
+    changeStockStream('subscribe', 'AAPL')
+    changeStockStream('subscribe', 'MSFT')
+    changeStockStream('subscribe', 'META')
+    changeStockStream('subscribe', 'VDC')
+    changeStockStream('subscribe', 'NFLX')
+    changeStockStream('subscribe', 'TGT')
+    changeStockStream('subscribe', 'GOOGL')
+    changeStockStream('subscribe', 'AMZN')
 });
 
-// Listen for messages
 socket.addEventListener('message', function (event) {
     var tmp = JSON.parse(event.data);
     if(tmp['data'] != null && tmp['data'].length > 0) {
@@ -24,7 +26,9 @@ socket.addEventListener('message', function (event) {
     }
 });
 
-// Unsubscribe
- var unsubscribe = function(symbol) {
-    socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+var changeStockStream = function(option, symbol) {
+    socket.send(JSON.stringify({
+        'type': option,
+        'symbol': symbol
+    }));
 }
